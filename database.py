890c3b6ae154ffbeb26a554e80d1856c6b110d21
Siddhardha Naidu Gorja <siddhardh23@gmail.com>
2026-05-18 11:50:56 +0200

    fixed postgres driver for render

--- database.py
+++ database.py
@@ -6,6 +6,11 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+DATABASE_URL = DATABASE_URL.replace(
+    "postgresql://",
+    "postgresql+psycopg://"
+)
+
 engine = create_engine(DATABASE_URL)
 
 print("Database connected successfully!")
