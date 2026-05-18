3e2672ce93b7a13c10763545f3a31035395ae831
Siddhardha Naidu Gorja <siddhardh23@gmail.com>
2026-05-18 09:54:25 +0200

    Added cloud database support

--- database.py
+++ database.py
@@ -4,16 +4,8 @@ import os
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
-DATABASE_URL = (
-    f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
-print("Database connection successful!")
+print("Database connected successfully!")
